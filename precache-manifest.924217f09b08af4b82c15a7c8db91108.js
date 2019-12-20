8edd98787043687e63542dd8466ae28d57178d80
thinman918 <627020231@qq.com>
2019-12-20 16:55:12 +0800

    add mix

---
+++ precache-manifest.924217f09b08af4b82c15a7c8db91108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af94d29f3d93918567ee",
+    "url": "/static/js/main.cbd365f3.chunk.js"
+  },
+  {
+    "revision": "fb5ab432d5dbbfcd3236",
+    "url": "/static/js/2.7be585af.chunk.js"
+  },
+  {
+    "revision": "af94d29f3d93918567ee",
+    "url": "/static/css/main.22dff5d7.chunk.css"
+  },
+  {
+    "revision": "08d600f82bcf9ce10f9f9fab4251dba9",
+    "url": "/index.html"
+  }
+];
